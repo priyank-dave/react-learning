da6752ba6fcd36bae7f4212dd4b183d3c048ba30
priyank-dave <priyankdave@fxis.ai>
2025-02-10 11:39:17 +0530

    Created usePopcorn and travel-list app.

--- 06 - State, Events, and Forms Interactive Components/steps/src/App.js
+++ 06 - State, Events, and Forms Interactive Components/steps/src/App.js
@@ -1,29 +1,72 @@
+import { useState } from "react";
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
   "Invest your new income 🤑",
 ];
 export default function App() {
-  const step = 1;
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className="active">1</div>
-        <div>2</div>
-        <div>3</div>
-      </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
-          {" "}
-          Previous{" "}
-        </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
-          Next{" "}
-        </button>
-      </div>
+    <div>
+      <Steps />
+      <Steps />
     </div>
+  );
+}
+
+function Steps() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  // const [test, setTest] = useState({ name: "Jonas" });
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+  function handleNext() {
+    if (step < 3) {
+      setStep(step + 1);
+      setStep(step + 1);
+    }
+
+    // BAD PRACTICE
+    // test.name = "Fred";
+
+    // GOOD PRACTICE
+    // setTest({ name: "Fred" });
+  }
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+            {/* {test.name} */}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              {" "}
+              Previous{" "}
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next{" "}
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
